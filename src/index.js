cc1974587e313d469f5ce8a9b647e748ac780607
Elena Kharsova <elena.kharsova@gmail.com>
2023-03-18 16:59:51 +0100

    Add cursor point

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// клик на кнопку order here=белый лист
+const buttonOrder = document.querySelector(".button-big");
+
+buttonOrder.addEventListener("click", () => {
+    alert("Order, please");
+});
+
+// buttonOrder.addEventListener("mouseenter", (event) => {
+//     event.target.style.background = "green";
+// })
+
+// buttonOrder.addEventListener("mouseleave", (event) => {
+//     event.target.style.background = "#2D635E";
+// })
