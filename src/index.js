7916d701e1921d4dccbda4b7d44d222d910c8701
Elena Kharsova <elena.kharsova@gmail.com>
2023-03-20 15:00:49 +0100

    correc message of alert

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-// клик на кнопку order here=белый лист
 const buttonOrder = document.querySelector(".button-big");
 
 buttonOrder.addEventListener("click", () => {
-    alert("you are fool yes yes yes");
+    alert("Order, please");
 });
