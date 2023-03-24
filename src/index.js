ee3c8c3377ad36e04c18bb8e91d2ab8ee2e3bc83
Elena Kharsova <elena.kharsova@gmail.com>
2023-03-24 17:40:57 +0100

    Add webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
+import "./styles.css";
+// import img from './images/cup.png';
+// import img from './images/facebook.png';
+// import img from './images/facebook.png';
+// import img from './images/glass.svg';
+// import img from './images/instagram.png';
+// import img from './images/Jarr.png';
+// import img from './images/Label.svg';
+// import img from './images/twitter.svg';
+
+
 const buttonOrder = document.querySelector(".button-big");
 
 buttonOrder.addEventListener("click", () => {
