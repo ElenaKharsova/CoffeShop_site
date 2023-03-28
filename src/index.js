3eb3244ac8ac6287a85e0b45b586d16f141f4d21
Elena Kharsova <elena.kharsova@gmail.com>
2023-03-28 15:38:21 +0200

    Add gitpages config

--- src/index.js
+++ src/index.js
@@ -1,13 +1,4 @@
 import "./styles.css";
-// import img from './images/cup.png';
-// import img from './images/facebook.png';
-// import img from './images/facebook.png';
-// import img from './images/glass.svg';
-// import img from './images/instagram.png';
-// import img from './images/Jarr.png';
-// import img from './images/Label.svg';
-// import img from './images/twitter.svg';
-
 
 const buttonOrder = document.querySelector(".button-big");
 
