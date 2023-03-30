7943b32faf7fb4deccbcd96def8cb0a4775c7f86
Elena Kharsova <elena.kharsova@gmail.com>
2023-03-30 15:51:43 +0200

    prettier install

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,23 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    "jest/globals": true,
   },
-  extends: 'airbnb-base',
-  overrides: [
-  ],
+  extends: ["airbnb-base", "prettier"],
+  overrides: [],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
+  plugins: ["jest"],
   rules: {
+    "max-len": [
+      "error",
+      {
+        ignoreComments: true,
+      },
+    ],
+    "import/prefer-default-export": "off",
+    "no-alert": "off",
   },
 };
--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import "./styles.css";
 const buttonOrder = document.querySelector(".button-big");
 
 buttonOrder.addEventListener("click", () => {
-    alert("Order, please");
+  alert("Order, please");
 });
