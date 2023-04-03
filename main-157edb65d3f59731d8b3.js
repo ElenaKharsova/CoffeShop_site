f7a1fe049acd8fd7f16f87c53de865ee11afe3f6
ElenaKharsova <96537658+ElenaKharsova@users.noreply.github.com>
2023-04-03 11:25:07 +0000

    Deploying to gh-pages from @ ElenaKharsova/CoffeShop_site@08e5ba23b4f02eaf9f26d3cca38fbbab8bfb87c3 🚀

---
+++ main-157edb65d3f59731d8b3.js
@@ -0,0 +1,2 @@
+!function(){"use strict";document.querySelector(".button-big").addEventListener("click",(function(){alert("Order, please!")}))}();
+//# sourceMappingURL=main-157edb65d3f59731d8b3.js.map
--- main-4bd5938cf42d657999dc.js
+++
@@ -1,2 +0,0 @@
-!function(){"use strict";document.querySelector(".button-big").addEventListener("click",(function(){alert("Order, please")}))}();
-//# sourceMappingURL=main-4bd5938cf42d657999dc.js.map
